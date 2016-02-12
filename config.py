1edf35d128b9ae55a8fcbdf94d6ed0b4c6a93914
Selim <selim.salman@mobileeventguide.de>
2016-02-12 14:21:30 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#url to get packages
+url = "https:"
+#set the temp directory to download the packages to; do not forget the trailing /
+tmp_dir = "/tmp/"
+
+#Specific Android config:
+path_to_sdk = "/Users/ssalman/Library/Android/sdk/platform-tools/"
